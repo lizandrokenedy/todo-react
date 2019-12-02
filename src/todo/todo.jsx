d22d75bf8bb6286972aff87ab3436cfa6afd15d1
Lizandro Kenedy <lizandrokenedy@gmail.com>
2019-12-02 17:41:26 -0400

    first commit

---
+++ src/todo/todo.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+const URL = "http://localhost:3000/todo"
+
+export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => console.log('Funcionou!'))
+    }
+
+    render() {
+        return (
+            <div>
+                <PageHeader name="Tarefas" small="Cadastro" />
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
+                <TodoList />
+            </div>
+        )
+    }
+}
